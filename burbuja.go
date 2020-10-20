97f6dad922b0a8a5f70f5e6a0a5b60b9150b18fb
Diego Osorio <juan.osorio1599@alumnos.udg.mx>
2020-10-20 14:21:25 -0500

    Primera prueba del ordenamiento burbuja

--- burbuja.go
+++ burbuja.go
@@ -1,9 +1,22 @@
 package main
 
-func Burbuja(s []int64)  {
 
+func Burbuja(s []int64) {
+
+	for i := 0; i < len(s); i++ {
+		for j := 0; j < len(s); i++ {
+			if (s[i] < s[j]) {
+
+				aux := s[i]
+				s[i] = s[j]
+				s[j] = aux
+
+			}
+		}
+	}
 }
 
-func main()  {
-	
+func main() {
+	s := []int64{}
+	Burbuja(s)
 }
